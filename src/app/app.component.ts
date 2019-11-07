aa7a6d0056f504a2cd2bbc0ecd66859164b6af60
eyal <azran4u@gmail.com>
2019-11-07 14:11:10 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Student } from './student.model';
+import { StudentService } from './student.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'angular-error-handling';
+export class AppComponent implements OnInit {
+
+    students: Student[] = [];
+    students$: Observable<Student[]>;
+
+    constructor(private studentservice: StudentService) {
+      this.students$ = studentservice.getStudents();
+    }
+
+    ngOnInit() {
+        const studentsObservable = this.studentservice.getStudents();
+        studentsObservable.subscribe((studentsData: Student[]) => {
+            this.students = studentsData;
+        });
+    }
 }
